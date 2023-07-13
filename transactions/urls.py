fdb4a1462cb5a2d044b3925b7e47efd7c076b987
Felipe López <felipeantonio.0804@gmail.com>
2023-07-13 02:47:22 -0600

    Cambio en orden de url, que no altera el resultado

--- transactions/urls.py
+++ transactions/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # * ex: /transactions/
     path('', views.index,name='index'),
     # * ex: /transactions/5
-    path('<int:category_id>/usodetag/url/con_app_name_view_name/', views.details,name='details'),
+    path('usodetag/url/con_app_name_view_name/<int:category_id>/', views.details,name='details'),
     # * ex: /transactions/details_dates/5
     path('details_dates/<int:category_id>/', views.details_dates,name='details_dates')
 ]
