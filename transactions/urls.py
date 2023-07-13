72511cb4c58cc81311a55325a6a69089e3cfc966
Felipe López <felipeantonio.0804@gmail.com>
2023-07-13 02:42:38 -0600

    Cambio en uso de etiqueta url, necesario definir variable 'app_name' en archivo urls.py de la aplicacion

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = "transactionsApp"
 urlpatterns = [
     # * ex: /transactions/
     path('', views.index,name='index'),
     # * ex: /transactions/5
-    path('<int:category_id>/', views.details,name='details'),
+    path('<int:category_id>/usodetag/url/con_app_name_view_name/', views.details,name='details'),
     # * ex: /transactions/details_dates/5
     path('details_dates/<int:category_id>/', views.details_dates,name='details_dates')
 ]
