317cd74e5099808f5b80b08600a27b85a1a763cc
Felipe López <felipeantonio.0804@gmail.com>
2023-07-12 23:56:25 -0600

    Agregar diferentes urls en aplicacion de 'transactions' con su respectiva vista, mandandole parametros por la url

--- transactions/urls.py
+++ transactions/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index,name='index')
+    # * ex: /transactions/
+    path('', views.index,name='index'),
+    # * ex: /transactions/5
+    path('<int:category_id>/', views.details,name='details'),
+    # * ex: /transactions/details_dates/5
+    path('details_dates/<int:category_id>/', views.details_dates,name='details_dates')
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -2,3 +2,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world! This is the home page. of transactions app")
+
+def details(request,category_id):
+    return HttpResponse(f"Estas viendo el detalle de la categoria {category_id}")
+
+def details_dates(request,category_id):
+    return HttpResponse(f"Estas viendo el detalle de la categoria por rango de fecha {category_id}")
