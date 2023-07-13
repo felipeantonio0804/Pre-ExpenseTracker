b7a3686bcb458e953d177e54d7b8eb1b414f740e
Felipe López <felipeantonio.0804@gmail.com>
2023-07-13 02:19:48 -0600

    Cambio a uso de funcion 'get_object_or_404' para el renderizado de page not found y template de details (forma de iterar en template objects.all sin parentesis)

--- transactions/views.py
+++ transactions/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Category,Transaction
 
@@ -9,7 +9,10 @@ def index(request):
     return render(request,'transactions/index.html',context)
 
 def details(request,category_id):
-    return HttpResponse(f"Estas viendo el detalle de la categoria {category_id}")
+    context = {
+        'category': get_object_or_404(Category,pk=category_id)
+    }
+    return render(request,'transactions/details.html',context)
 
 def details_dates(request,category_id):
     return HttpResponse(f"Estas viendo el detalle de la categoria por rango de fecha {category_id}")
