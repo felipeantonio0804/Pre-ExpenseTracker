d38dede6a699dfbd72729f46afe73cca6ab21e25
Felipe López <felipeantonio.0804@gmail.com>
2023-07-12 19:03:00 -0600

    Agregar los modelos al admin.py para que pueda ser administrado estos modelos y cambiar definion de __str__ de modelo Transaction

--- transactions/models.py
+++ transactions/models.py
@@ -4,15 +4,22 @@ class Category(models.Model):
     name = models.CharField('Category Name', max_length=50)
     description = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.name
+
 class TypeTransaction(models.IntegerChoices):
     EXPENSE = 0, 'Expense'
     REVENUE = 1, 'Revenue'
 
 class Transaction(models.Model):
     category = models.ForeignKey('Category', on_delete=models.RESTRICT)
-    date = models.DateField('date transaction')
+    date = models.DateField('date transaction',auto_now=True)
     amount = models.DecimalField(max_digits=5,decimal_places=2)
     note = models.CharField(max_length=250)
     type = models.IntegerField(default=TypeTransaction.EXPENSE, choices=TypeTransaction.choices)
 
-
+    def __str__(self):
+        if (self.type == TypeTransaction.EXPENSE):
+            return "Gasto, {} {}".format(self.note,self.amount)
+        elif(self.type == TypeTransaction.REVENUE):
+            return "Ingreso, {} {}".format(self.note,self.amount)
