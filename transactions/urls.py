46d28de2b13ea206825497b3241bb6f6c7089333
Felipe López <felipeantonio.0804@gmail.com>
2023-07-13 06:04:29 -0600

    Agregar logica dentro de view.py para incrementar el conteo de las consultas de las transacciones, redifiniendo nuevamente algunos nombres de rutas y funciones de view

--- transactions/urls.py
+++ transactions/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('<int:category_id>/', views.details,name='details'),
     # * ex: /transactions/details_category/5
     path('details_category/<int:category_id>/', views.details_category,name='details_category'),
-     # * ex: /transactions/details_dates/transaction/5
-    path('details_counter/transaction/<int:category_id>/', views.details_results_transaction,name='details_results_transaction')
+     # * ex: /transactions/details_consult_transaction/5
+    path('details_consult_transaction/<int:category_id>/', views.details_consult_transaction,name='details_consult_transaction'),
+     # * ex: /transactions/details_results_category/5
+    path('details_results_category/<int:category_id>/', views.details_results_category,name='details_results_category'),
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -21,5 +21,22 @@ def details_category(request,category_id):
     }
     return render(request,'transactions/details_category.html',context)
 
-def details_results_transaction(request,category_id):
+def details_consult_transaction(request,category_id):
+    category = get_object_or_404(Category,pk=category_id)
+    try:
+        #Toma el valor de value del input, en base al name, request.POST['transaction']
+        transaction = category.transaction_set.get(pk=request.POST['transaction'])
+    #Controlar estos 2 tipos de errores
+    except (KeyError,Transaction.DoesNotExist):
+        context = { 
+            'category': category,
+            'error_message':'No se ha seleccionado una transacción antes de mandar a consultar.'
+        }
+        return render(request,'transactions/details_category.html',context)
+    else:
+        transaction.counter += 1
+        transaction.save()
+        return HttpResponseRedirect(reverse('transactionsApp:details_results_category',args=(category.id,)))
+    
+def details_results_category(request,category_id):
     return HttpResponse(f"Estas viendo el detalle de la categoria {category_id} para ver cuantas veces ha sido consultada esta transaccion por el contador")
