be1ae25bd61749b373096ae0e3000e057a694214
Felipe López <felipeantonio.0804@gmail.com>
2023-07-14 16:24:23 -0600

    Agregando personalizacion a la administracion de los modelos a traves del admin.py

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 from .models import Category,Transaction
 
-admin.site.register([Category,Transaction])
+class TransactionTabularInline(admin.TabularInline):
+    model = Transaction
+    extra = 3
+
+class TransactionStackedInline(admin.StackedInline):
+    model = Transaction
+    extra = 2
+
+class CategoryAdmin(admin.ModelAdmin):
+    #Definiendo el orden de los campos al crear un registro nuevo
+    fields = ['description','name']
+    inlines=[TransactionTabularInline,TransactionStackedInline]
+    list_display = ('name','description')
+    search_fields = ['description']
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('category','date','note','amount','type','is_recent_date_of_transaction')
+    list_filter = ['date']
+    search_fields = ['note']
+    exclude= ['counter']
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Transaction,TransactionAdmin)
 
